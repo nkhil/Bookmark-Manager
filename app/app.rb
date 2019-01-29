cb15ddfdf488a6caec20cb37d245d059542a71cf
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-29 09:43:03 +0000

    add bookmarks route to app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
+require_relative './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
   end
 
   run! if app_file == $0
