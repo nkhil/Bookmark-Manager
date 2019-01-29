e1ea9f749a5cd71979a60763841f3ee6da77c79d
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-29 09:42:23 +0000

    modify Capybara feature test

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -3,4 +3,13 @@ feature 'Capybara tests' do
     visit('/')
     expect(page).to have_content('Bookmark Manager')
   end
+
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit('/bookmarks')
+      expect(page).to have_content 'http://www.google.com'
+      expect(page).to have_content 'http://www.destroyallsoftware.com'
+      expect(page).to have_content 'http://www.makersacademy.com'
+    end
+  end
 end
