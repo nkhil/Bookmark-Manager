8a4fcfdec329ac81d1a561a68e002fa86abee724
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-31 11:31:49 +0000

    modify app.rb

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 require_relative './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  enable :sessions
+  enable :sessions, :method_override
 
   get '/' do
     erb :index
@@ -10,7 +10,25 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
+    # p @bookmarks
     erb :'bookmarks/index'
+  end
+
+  get '/bookmarks/new' do
+    erb :'bookmarks/new'
+  end
+
+  post '/bookmarks-process' do
+    p "form data submitted to /bookmarks"
+    p params
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+  delete '/bookmarks/:id' do
+    p params
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
