aa70a60c839c20d266729762f5d97557e1f93e30
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-31 11:32:46 +0000

    add tests and passing code for bookmarks.rb

--- app/lib/bookmark.rb
+++ app/lib/bookmark.rb
@@ -1,14 +1,49 @@
 require 'pg'
 
 class Bookmark
-  def self.all
+
+  attr_reader :id, :title, :url
+
+  def initialize(id:, title:, url:)
+    @id = id
+    @title = title
+    @url = url
+  end
+
+  def self.all 
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'bookmark_manager_test')
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    result = connection.exec("SELECT * FROM bookmarks")
-    result.map { |bookmark| bookmark['url'] }
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| 
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) 
+    }
+  end
+
+  def self.create(url:, title:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    # connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}','#{url}') RETURNING id, url, title;")
+    result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+    # p "#{title} and #{url} added"
+  end
+
+  def self.delete(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    connection.exec("DELETE FROM bookmarks WHERE id = #{id};")
+
   end
 end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -6,15 +6,34 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Adding test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+   
       bookmarks = Bookmark.all
+   
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      # expect(bookmarks.first.id).to eq bookmark['id']
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
+    end
+  end
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+  describe '.create' do
+    it 'creates a new bookmark' do
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+
+      expect(bookmark.url).to eq('http://www.testbookmark.com')
+      expect(bookmark.title).to eq('Test Bookmark')
+    end
+  end
+
+  describe '.delete' do
+    it 'deletes a bookmark' do
+      bookmark = Bookmark.create(url: 'http://www.james.com', title: 'James')
+      Bookmark.delete(id: bookmark.id)
+      expect(Bookmark.all.length).to eq(0)
     end
   end
 end
