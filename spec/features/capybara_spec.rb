de79cc488c9996bb199f84be73410357fcc1a060
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-01 14:58:19 +0000

    add feature test for 'updating bookmark'

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -1,4 +1,5 @@
 feature 'Capybara tests' do
+  
   scenario 'The homepage displays the correct information' do
     visit('/')
     expect(page).to have_content('Bookmark Manager')
@@ -31,6 +32,33 @@ feature 'Capybara tests' do
       fill_in('title', with: 'Test Bookmark')
       click_button('Submit')
       expect(page).to have_link('Test Bookmark', href: 'http://testbookmark.com')
+    end
+  end
+
+  feature 'Updating a bookmark' do
+    scenario 'A user can update a bookmark' do
+      bookmark = Bookmark.create(url: 'http://www.james.com', title: 'James')
+      visit('/bookmarks')
+      expect(page).to have_link('James', href: 'http://www.james.com')
+      first('.bookmark').click_button('Edit')
+      expect(current_path).to eq("/bookmarks/#{bookmark.id}/edit")
+      fill_in('url', with: 'http://www.google.com')
+      fill_in('title', with: 'James Rudrum')
+      click_button('Submit')
+      expect(current_path).to eq('/bookmarks')
+      expect(page).not_to have_link('James', href: 'http://www.james.com')
+      expect(page).to have_link('James Rudrum', href: 'http://www.google.com')
+    end
+  end
+
+  feature 'Deleting a bookmark' do
+    scenario 'A user can delete a bookmark' do
+      Bookmark.create(url: 'http://www.google.com', title: 'Google')
+      visit('/bookmarks')
+      expect(page).to have_link('Google', href: 'http://www.google.com')
+      first('.bookmark').click_button 'Delete'
+      expect(current_path).to eq('/bookmarks')
+      expect(page).not_to have_link('Google', href: 'http://www.google.com')
     end
   end
   
