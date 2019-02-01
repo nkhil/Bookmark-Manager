1884c347556579205c22cb14c92b220c48de688e
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-01 14:53:28 +0000

    refactor bookmark to use DatabaseConnection

--- app/lib/bookmark.rb
+++ app/lib/bookmark.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require_relative './database_connection'
 
 class Bookmark
 
@@ -11,39 +12,28 @@ class Bookmark
   end
 
   def self.all 
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = connection.exec("SELECT * FROM bookmarks;")
+    result = DatabaseConnection.query("SELECT * FROM bookmarks")
     result.map { |bookmark| 
       Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) 
     }
   end
 
   def self.create(url:, title:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    # connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}','#{url}') RETURNING id, url, title;")
-    result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    result = DatabaseConnection.query("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
-    # p "#{title} and #{url} added"
   end
 
   def self.delete(id:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
+    DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id};")
+  end
 
-    connection.exec("DELETE FROM bookmarks WHERE id = #{id};")
+  def self.update(id:, url:, title:)
+    result = DatabaseConnection.query("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
 
+  def self.find(id:)
+    result = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{id};")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
